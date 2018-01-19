504f66fb2513df7af2e8f8f84e9f39394fc8feca
Mariah Shipley <mshipley57@gmail.com>
2018-01-19 10:48:09 -0800

    got js button working

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("form#beepForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("")
+
+
+//var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//for (var index = 0; index < numbers.length; index += 1) {
+  //alert('beep!');
+//}
