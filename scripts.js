fb66c08b50db6c1801bd223587ce659ea151e7ac
Mariah Shipley <mshipley57@gmail.com>
2018-01-19 11:08:15 -0800

    page responds properly

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   $("form#beepForm").submit(function(event) {
     event.preventDefault();
-    var userInput = $("")
+    var userInput = $("input#sentence").val();
+    //var number = array.toString()
+    if (userInput === 0) {
+         alert("yes!");
+       } else (userInput !=5)
+         alert("No way, Jose.");
 
-
-//var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-//for (var index = 0; index < numbers.length; index += 1) {
-  //alert('beep!');
-//}
+  });
+});
