7f9664b91c1140c4f84cc177496e3a247b489083
Mariah Shipley <mshipley57@gmail.com>
2018-01-19 16:09:21 -0800

    project is only semi functional

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
   $("form#beepForm").submit(function(event) {
-    event.preventDefault();
-    var userInput = $("input#sentence").val();
-    //var number = array.toString()
-    if (userInput === 0) {
-         alert("yes!");
-       } else (userInput !=5)
-         alert("No way, Jose.");
+  event.preventDefault();
+  var userInput = $("input#sentence").val();
+  //var ones = ["1", "10", "11", "12","13", "14", "15", "16", "17", "18", "19", "21"]
+  for (var i = 1; i <= 121; i ++) {
+    alert(i);
+    if ( i % 3 == 0)
+      alert("beep!");
+  }
+    //else if (i === 0) {
+      //alert("boop!");
 
-  });
+});
 });
